8e01812fe99a8a355bb48e7a32acf119ba710ade
Sanmati Dugad <106019002+sanmatidugad@users.noreply.github.com>
2024-03-01 19:10:28 -0500

    Create Population Census.sql

---
+++ Basic JOIN/Population Census.sql
@@ -0,0 +1,6 @@
+-- Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'. 
+-- Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+
+SELECT SUM(CITY.POPULATION)
+FROM CITY INNER JOIN COUNTRY ON  CITY.CountryCode = COUNTRY.Code
+WHERE COUNTRY.CONTINENT = 'Asia'
